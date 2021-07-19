f3b6175ba67cace21709e553b29e25fb8a1a60ec
GNUOverYooouu <dtortoise99@gmail.com>
2021-07-19 13:30:10 -0700

    Coming soon: code...

---
+++ src/kernel/init/main.c
@@ -0,0 +1,8 @@
+/************************************************************
+ * The main kernel file, where everything comes together.   *
+ * (c) 2021 monkelab                                        *
+ ************************************************************/
+
+void kernel_main(void) {
+  /* Coming soon: Code... */
+}
