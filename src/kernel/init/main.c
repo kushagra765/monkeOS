b17b52d76364046f916b123adf5d58fe2c758bdb
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-09-08 20:38:39 +0530

    Updated

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 #include <kernel/tty.h>
 
 void kernel_main(void) {
-	terminal_initialize();
-	printf("Welcome to monkeOS!\n");
-	printf("This is all there is :(");
+  terminal_initialize();
+  printf("Welcome to monkeOS!\n");
+  printf("This is all there is :(");
 }
