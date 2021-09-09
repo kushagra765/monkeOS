33437b9d945fab58831eded4df7a4902678a6523
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-08 17:08:43 -0700

    Yay

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
 #include <kernel/tty.h>
+#include <kernel/log.h>
+#include <stdio.h>
 
 void kernel_main(void) {
   terminal_initialize();
+  log("TTY initialized");
+  log("Kernel booted");
   printf("Welcome to monkeOS!\n");
   printf("This is all there is :(\n");
   printf("At least it works.");
